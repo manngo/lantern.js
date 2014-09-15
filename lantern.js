b70af92aff8fffcd2d0d8512e5877324c3b27a13
manngo <mark@imac.mark.manngo.net.1.168.192.in-addr.arpa>
2014-09-15 17:37:26 +1000

    More Options, Fancy CSS

--- lantern.js
+++ lantern.js
@@ -40,6 +40,8 @@
 			title:		'title',		//	id of title
 			showing:	'showing',		//	attribute when showing
 			escape:		false,			//	whether responds to escape key
+			dblclick:	false,			//	whether double-click required
+			draggable:	false,			//	whether to allow dragging
 			delay:		0,				//	delay (ms) before hiding
 		};
 
@@ -71,6 +73,9 @@
 			options.escape=!!options.escape;
 			options.delay=parseInt(options.delay,10);
 
+			options.dblclick=!!options.dblclick;
+			options.draggable=!!options.draggable;
+
 		/*
 			================================ */
 
@@ -79,10 +84,16 @@
 
 		background=document.createElement('div');
 		background.setAttribute('id',options.background);
-		background.onclick=hide;
 
 			div=document.createElement('div');
 			div.setAttribute('id',options.div);
+			if(options.draggable) {
+				draggable();
+				div.ondblclick=doCentre;
+			}
+
+			if(options.dblclick)  background.ondblclick=hide;
+			else background.onclick=hide;
 
 			background.appendChild(div);
 
@@ -99,10 +110,19 @@
 
 		if(!images.length) return false;
 		for(i=0;i<images.length;i++) {
-			images[i].onclick=function(e) {
-				show(this);
-				return false;
-			};
+			if(options.dblclick) {
+				images[i].onclick=function(e) {return false; };
+				images[i].ondblclick=function(e) {
+					show(this);
+					return false;
+				};
+			}
+			else {
+				images[i].onclick=function(e) {
+					show(this);
+					return false;
+				};
+			}
 		}
 
 		function show(a) {
@@ -113,8 +133,9 @@
 			title.textContent='';
 			div.style.width=div.clientWidth+'px';
 			title.textContent=a.querySelector('img').title||a.querySelector('img').alt;
-			div.style.left = (window.innerWidth - div.offsetWidth)/2 + 'px';
-			div.style.top = (window.innerHeight - div.offsetHeight)/2 + 'px';
+
+			if(!div.dragged) doCentre();
+
 			background.setAttribute(options.showing,true);
 			div.setAttribute(options.showing,true);
 			img.setAttribute(options.showing,true);
@@ -143,5 +164,81 @@
 				background.style.display='none';
 			}
 		}
+		function doCentre() {
+			div.style.left = (window.innerWidth - div.offsetWidth)/2 + 'px';
+			div.style.top = (window.innerHeight - div.offsetHeight)/2 + 'px';
+		}
+
+		function draggable() {
+//			element.style.position='absolute';
+			div.activateDrag=function() {
+				div.onmousedown=startDrag;
+			};
+
+			function startDrag(event) {
+				div.dragged=true;
+				//	Element Position
+					div.left=div.offsetLeft;
+					div.top =div.offsetTop;
+				//	Mouse Position
+					div.startX=event.clientX;
+					div.startY=event.clientY;
+
+				//	Enable Drag & Drop Events
+					document.onmousemove=drag;
+					document.onmouseup=release;
+
+				//	Change Appearance
+					div.style.cursor='move';
+					div.style.opacity='.60';
+					div.style.filter='alpha(opacity=60)';
+
+				return false;
+			}
+
+			function drag(e) {
+				var event=e||window.event;
+				div.style.left=div.left + event.clientX - div.startX + 'px';
+				div.style.top =div.top  + event.clientY - div.startY + 'px';
+				return false;
+			}
+
+			function release(e) {
+				document.onmousemove=null;
+				document.onmouseup=null;
+				div.style.opacity=div.style.filter=null;
+				div.style.cursor=null;
+			}
+
+			div.activateDrag();
+
+		}
 
 	}
+
+/*	Function say
+	================================================
+
+	Display message on screen
+
+	================================================ */
+
+
+		function say(message) {
+            var div=document.createElement('div');
+            //	div.style.cssText='';
+            div.setAttribute('id','message');
+
+            div.style.cssText='width: 200px; height: 200px;\
+                overflow: auto; position: fixed;\
+                right: 20px; top: 20px; white-space: pre-wrap;\
+                border: thin solid #666;\
+                box-shadow: 4px 4px 4px #666;\
+                padding: 1em; font-family: monospace;';
+
+            document.body.appendChild(div);
+            say=function(message) {
+                div.textContent+=message+'\n';
+            };
+            say(message);
+        }
